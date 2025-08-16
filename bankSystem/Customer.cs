ee4c09991240ddbfe7cd9775041d91035a130620
Sara Ibrahim Mostafa <saraaaaaaasaaaaaaa@gmail.com>
2025-08-16 22:13:46 +0300

    BankSystem

---
+++ bankSystem/Customer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bankSystem
+{
+   
+        public class Customer
+        {
+            private static int _seq = 1000;
+
+            public string CustomerId { get; }
+            public string NationalId { get; }
+            public DateTime DateOfBirth { get; set; }
+            public string FullName { get; set; }
+
+            public Customer(string fullName, string nationalId, DateTime dateOfBirth)
+            {
+                FullName = string.IsNullOrWhiteSpace(fullName) ? throw new ArgumentException("Full name required") : fullName.Trim();
+                NationalId = string.IsNullOrWhiteSpace(nationalId) ? throw new ArgumentException("National ID required") : nationalId.Trim();
+                DateOfBirth = dateOfBirth;
+
+                CustomerId = $"C{_seq++}";
+            }
+        }
+    }
---
+++ bankSystem/transaction.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bankSystem
+{
+   
+        public enum TransactionType
+        {
+            Deposit,
+            Withdrawal,
+            TransferOut,
+            TransferIn
+        }
+
+        public class Transaction
+        {
+            private static long _seq = 1;
+            public string TransactionId { get; }
+            public TransactionType Type { get; }
+            public decimal Amount { get; }
+            public DateTime DateUtc { get; }
+            public string Description { get; }
+            public decimal BalanceAfter { get; }
+            public string? FromAccountNumber { get; set; }
+            public string? ToAccountNumber { get; set; }
+
+            public Transaction(TransactionType type, decimal amount, DateTime dateUtc, string description, decimal balanceAfter,
+                string? fromAccountNumber = null, string? toAccountNumber = null)
+            {
+                TransactionId = $"TX{_seq++}";
+                Type = type;
+                Amount = amount;
+                DateUtc = dateUtc;
+                Description = description ?? "";
+                BalanceAfter = balanceAfter;
+                FromAccountNumber = fromAccountNumber;
+                ToAccountNumber = toAccountNumber;
+            }
+        }
+    }
